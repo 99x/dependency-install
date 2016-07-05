0ff7deec33e4d5a98c8e38fc26f08b32cf286ec4
Rumesh Eranga <rehrumesh@hotmail.com>
2016-07-06 01:48:41 +0530

    Added callback support upon completion

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ var vfs = require('vinyl-fs'),
 var isWin = /^win/.test(process.platform),
     commandSeparator = isWin ? '&' : ';';
 
-var dependancyInstall = function(path) {
+var dependancyInstall = function(path, callback) {
     var options = {
         dir: function(filePath) {
             return filePath.replace('package.json', '');
         }
     };
 
-    vfs.src([ path + '/**/package.json', '!./**/{node_modules,node_modules/**}'])
+    var stream = vfs.src([ path + '/**/package.json', '!./**/{node_modules,node_modules/**}'])
         .pipe(exec('(cd <%= options.dir(file.path) %> ' + commandSeparator + ' npm install) ' + commandSeparator
             + ' echo Installed for: <%= file.path %> \n', options))
         .pipe(exec.reporter());
+    stream.on('finish', callback);
 };
 module.exports = dependancyInstall
