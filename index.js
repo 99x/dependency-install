e67860b1c6503cd4848f06f6709fbcf6a1a1bdd7
Rumesh Hapuarachchi <rehrumesh@hotmail.com>
2016-07-06 08:47:31 +0530

    Handle issues when there are no callback provided

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 'use strict';
+
 var vfs = require('vinyl-fs'),
-    exec = require('gulp-exec');
+    exec = require('gulp-exec'),
+    isWin = /^win/.test(process.platform),
+    commandSeparator = isWin ? '&' : ';',
+    dependancyInstall = function(path, callback) {
+        var options = {
+            dir: function(filePath) {
+                return filePath.replace('package.json', '');
+            }
+        };
 
-var isWin = /^win/.test(process.platform),
-    commandSeparator = isWin ? '&' : ';';
+        var stream = vfs.src([path + '/**/package.json', '!./**/{node_modules,node_modules/**}'])
+            .pipe(exec('(cd <%= options.dir(file.path) %> ' + commandSeparator + ' npm install) ' + commandSeparator + ' echo Installed for: <%= file.path %> \n', options))
+            .pipe(exec.reporter());
 
-var dependancyInstall = function(path, callback) {
-    var options = {
-        dir: function(filePath) {
-            return filePath.replace('package.json', '');
-        }
+        stream.on('finish', callback || function() {
+            console.log("Package installation completed.");
+        });
     };
 
-    var stream = vfs.src([ path + '/**/package.json', '!./**/{node_modules,node_modules/**}'])
-        .pipe(exec('(cd <%= options.dir(file.path) %> ' + commandSeparator + ' npm install) ' + commandSeparator
-            + ' echo Installed for: <%= file.path %> \n', options))
-        .pipe(exec.reporter());
-    stream.on('finish', callback);
-};
 module.exports = dependancyInstall
